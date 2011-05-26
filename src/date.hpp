749c9985fc34d957a456148b23129cdc9810174f
Mekeor Melire <mekeor@ymail.com>
2011-05-26 23:13:13 +0200

    updated license and email-address

--- src/date.hpp
+++ src/date.hpp
@@ -1,15 +1,15 @@
 /**
  * get_localdate.hpp
  * 
- * Copyright 2010 Florian Weber <florian.johannes.weber@googlemail.com>
+ * Copyright 2010 Florian Weber <florian.weber@sfz-bw.de>
  * 
  * This program is free software; you can redistribute it and/or modify
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Affero General Public License for more details.
  * 
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Affero General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02110-1301, USA.
